96ccda4e42692b1ce9bf903bb0f2d5a15172f6e8
eshwagadkar <eshwagadkar007@gmail.com>
2025-10-29 09:04:27 +0530

    downgrade mongoose to use mongoose-unique-validator plus create a user model

---
+++ models/user.js
@@ -0,0 +1,14 @@
+import mongoose, { Schema } from 'mongoose'
+import mongooseUniqueValidator from 'mongoose-unique-validator'
+
+const userSchema = new Schema({ 
+    name: { type: String, required: true },
+    email: { type: String, required: true, unique: true },
+    password: { type: String, required: true, minLength: 6 },
+    image:{ type: String, required: true },
+    places:{ type: String, required: true }
+})
+
+userSchema.plugin(mongooseUniqueValidator)
+
+export default mongoose.model('User', userSchema)
