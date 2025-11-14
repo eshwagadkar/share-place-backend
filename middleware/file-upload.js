0f24424e465071ae0c139107ed3fb9fcc78373e5
eshwagadkar <eshwagadkar007@gmail.com>
2025-11-14 11:54:28 +0530

    install multer and configure it

---
+++ middleware/file-upload.js
@@ -0,0 +1,32 @@
+import multer from 'multer'
+import { v4 as uuidv4 } from 'uuid'
+
+const MIME_TYPE_MAP = {
+    'image/png': 'png',
+    'image/jpg': 'jpg',
+    'image/jpeg': 'jpeg'
+}
+
+// Disk storage
+const storage = multer.diskStorage({
+    
+    destination: (req, file, cb) => {
+        cb(null, 'uploads/images')
+    }, 
+
+    filename: (req, file, cb) => {
+        const ext = MIME_TYPE_MAP[file.mimetype]
+        cb(null, uuidv4() + '.' + ext)
+    }
+})
+
+
+export const fileUpload = multer({
+    limits: 500000, // 5kb
+    storage: storage,
+    fileFilter: (req, file, cb) => {
+        const isValid = !!MIME_TYPE_MAP[file.mimetype]
+        const error = isValid ? null : new Error('Invalid mime type!')
+        cb(error, isValid)
+    }
+})
