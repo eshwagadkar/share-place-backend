238c3d8bcbf6d04215184a3b33d41e451354d10a
eshwagadkar <eshwagadkar007@gmail.com>
2025-11-14 11:56:09 +0530

    use the middleware in the route

--- routes/user-routes.js
+++ routes/user-routes.js
@@ -3,22 +3,24 @@ import { getUsers,
          signUp, 
          signIn 
 } from '../controllers/users-controller.js'
+import { fileUpload } from '../middleware/file-upload.js'
  import { check } from 'express-validator'
 
 const router = Router()
 
 router.get('/', getUsers)
 
-router.post('/signup', [
-    check('name')
-    .not().isEmpty(),
-    check('email')
-    .normalizeEmail()
-    .isEmail(),
-    check('password')
-    .isLength({ min: 6 })
-
-], signUp)
+router.post('/signup',
+    fileUpload.single('image')
+    [
+     check('name')
+     .not().isEmpty(),
+     check('email')
+     .normalizeEmail()
+     .isEmail(),
+     check('password')
+     .isLength({ min: 6 })
+    ], signUp)
 
 router.post('/signin', [
      check('email')
